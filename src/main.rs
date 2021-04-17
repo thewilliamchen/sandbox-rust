dcd05da29ee7c6e7fa7976a5e2c03cce4602fad0
Will Chen <will@willchen.dev>
2021-04-17 13:04:07 -0400

    Rust bringup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+mod clazz;
 fn main() {
+    let a = 69;
     println!("Hello, world!");
+    print!("{}", test());
+}
+
+fn test() -> i32 {
+    0
+}
+
+fn me(space: i32) -> bool {
+    match space {
+        69 => {
+            return true;
+        }, _ => {
+            return false;
+        }
+    }
+}
+
+
+fn <'a> lifetime('a ) -> {
+
 }
